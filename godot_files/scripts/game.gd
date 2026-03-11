ddd4b29337582261d579f4bb8c9749ed256d26b5
night360isdead <night360isdead@gmail.com>
2026-03-11 15:11:26 +0600

    added the AI for when the human player goes first.

--- godot_files/scripts/game.gd
+++ godot_files/scripts/game.gd
@@ -7,6 +7,16 @@ extends Control
 #shows the current player. also the starting point of the game
 var current_player = "X"
 
+
+#the player and the ai symbol
+
+var human_player = "X"
+var ai_player = "O"
+
+
+
+
+
 #this shows who is the winner, O or X
 var winner = ""
 
@@ -18,21 +28,19 @@ var board_state = [
 ]
 
 #this is for determining the draw time
-var moves: int
+var moves: int = 0
 
 
 
 #this stores all 9 buttons and lets it access their properties
 var button_position = []
 
-#this is a variable for the AI, It will handle two functions, one for when the player is first other for when the computer if first
-var AI
+
 
 
 ##########################################################################################################3333
 func _ready() -> void:
-	#which one is first
-	AI = Callable(self,"ai_player_is_first")
+	
 	
 	
 	
@@ -60,16 +68,24 @@ func _on_button_pressed(button) -> void:
 		button.text = current_player
 		
 		draw()
-		
+		if moves == 9:
+			return
 		check_winner()
+		if winner != "":
+			return
 		
 		switch_player()
-		#$WaitTime.start()
-		#await $WaitTime.timeout
-		#AI.call()
-		#check_winner()
-		#
-		#switch_player()
+		best_move()
+		
+		draw()
+	
+		check_winner()
+		if winner != "":
+			return
+		switch_player()
+		
+		
+		
 		#prints the variable assigned in the win functions
 		print(winner)
 ###############################################################################################3
@@ -92,6 +108,7 @@ func draw():
 	if moves ==9:
 		print("game over")
 		stop1()
+	return
 
 func stop1():
 	animation_player.play("stop_animation")
@@ -106,8 +123,15 @@ func check_winner():
 	row_win()
 	col_win()
 	diag_win()
-
+	
 ######################
+
+
+
+
+
+
+
 
 
 #check for rows
@@ -168,21 +192,110 @@ func stop():
 
 
 #one ui control
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
 
 
 
-func ai_player_is_first():
-	pass
 
 
 
 
 
 
+#################################
 
-func ai_comp_is_first():
-	pass
+#this is for the minimax algorithm simulation
+func get_winner():
+	
+	#detecting the rows
+	for i in range(3):
+		if board_state[i][0] != "" and board_state[i][0] == board_state[i][1] and board_state[i][1] == board_state[i][2]:
+			return board_state[i][0]
+
+	# detecting the columns
+	for i in range(3):
+		if board_state[0][i] != "" and board_state[0][i] == board_state[1][i] and board_state[1][i] == board_state[2][i]:
+			return board_state[0][i]
+
+	# detecting the diagonals
+	if board_state[0][0] != "" and board_state[0][0] == board_state[1][1] and board_state[1][1] == board_state[2][2]:
+		return board_state[0][0]
+
+	if board_state[0][2] != "" and board_state[0][2] == board_state[1][1] and board_state[1][1] == board_state[2][0]:
+		return board_state[0][2]
+
+	return ""
+
+
+#this will check if the board is full or not.
+
+
+func is_board_full():
+	for i in range (3):
+		for j in range(3):
+			if board_state[i][j] == "":
+				return false
+	return true
+
+
+
+
+
+#calculated the best possible move
+func best_move():
+	var best_score = -INF
+	var move 
+	for i in range(3):
+		for j in range(3):
+			if board_state[i][j] == "":
+				board_state[i][j] = ai_player
+				var score = minimax(false)
+				board_state[i][j] = ""
+				if score > best_score:
+					best_score = score
+					move = Vector2i (i,j)
+	
+	board_state[move.x][move.y] = ai_player
+	button_position[move.x][move.y].text = ai_player
+
+
+
+
+#############################################################################333
+#minimax algorithm
+func minimax (is_maximizing):
+	#stops the recursion and returns the outcome for the particular spot
+	var result = get_winner()
+	if result == ai_player:
+		return 1
+	if result == human_player:
+		return -1
+	if is_board_full():
+		return 0
+	
+	if is_maximizing:
+		var best_score = -INF
+		for i in range (3):
+			for j in range (3):
+				if board_state[i][j] == "":
+					board_state[i][j] = ai_player
+					var score = minimax(false)
+					board_state[i][j] = ""
+					best_score = max(score,best_score)
+		return best_score
+	else:
+		var best_score = INF
+		for i in range (3):
+			for j in range (3):
+				if board_state[i][j] == "":
+					board_state[i][j] = human_player
+					var score = minimax(true)
+					board_state[i][j] = ""
+					best_score = min(score,best_score)
+		return best_score
+
+	
+	
 	
